5ae1655e109af98c2cf67c7f39d375548cc3c84a
David Pischke <dpischke@seg-gesundheitswesen.de>
2020-02-03 09:04:40 +0100

    Initial

---
+++ SpringFramwork5Test/src/main/java/com/springframework/SpringFramwork5Test/SpringFramwork5TestApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.SpringFramwork5Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramwork5TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFramwork5TestApplication.class, args);
+	}
+
+}
---
+++ SpringFramwork5Test/src/test/java/com/springframework/SpringFramwork5Test/SpringFramwork5TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.SpringFramwork5Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramwork5TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
